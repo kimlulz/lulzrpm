51e293a2240fb5ccf72c8e724381f83b4f2741aa
kimlulz <kimlulz@naver.com>
2021-08-02 17:52:05 +0900

    Updated Script

--- d2d_gnome40.sh
+++ d2d_gnome40.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo dnf install -y gnome-tweaks sassc
+sudo dnf install -y gnome-tweaks sassc flatpak
 git clone https://github.com/ewlsh/dash-to-dock/
 cd dash-to-dock
 git checkout ewlsh/gnome-40
 git pull
 make
 sudo make install
+wget https://dl.flathub.org/repo/appstream/org.gnome.Extensions.flatpakref
+flatpak install org.gnome.Extensions.flatpakref
