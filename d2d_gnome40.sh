dcd67c815e895e78ca9b3788e967a2b2ecc0729b
kimlulz <kimlulz@naver.com>
2021-08-02 17:48:35 +0900

    Updated Script

--- d2d_gnome40.sh
+++ d2d_gnome40.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-## Gnome 40 ONLY
-if has_command apt; then
-    sudo apt install -y gnome-tweaks gettext
-  elif has_command dnf; then
-    sudo dnf install -y gnome-tweaks sassc
-  fi
+sudo dnf install -y gnome-tweaks sassc
 git clone https://github.com/ewlsh/dash-to-dock/
 cd dash-to-dock
 git checkout ewlsh/gnome-40
