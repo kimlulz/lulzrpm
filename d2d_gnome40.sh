ef2729fb1c5c726774f44c69ae86b27fd0a14e5a
kimlulz <kimlulz@naver.com>
2021-08-03 10:17:37 +0900

    Updated

--- d2d_gnome40.sh
+++ d2d_gnome40.sh
@@ -5,7 +5,7 @@ cd dash-to-dock
 git checkout ewlsh/gnome-40
 git pull
 make
-sudo make install
+make install
 wget https://dl.flathub.org/repo/appstream/org.gnome.Extensions.flatpakref
 sudo flatpak install -y org.gnome.Extensions.flatpakref
 echo "Finished.. Reboot and run Gnome Extensions to enable D2D"
