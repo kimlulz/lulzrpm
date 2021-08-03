1909ee62fd9c6f11042fe1feb69d9f133b5a1c00
kimlulz <kimlulz@naver.com>
2021-08-03 10:22:08 +0900

    Updated

--- lulzrpm.sh
+++ lulzrpm.sh
@@ -3,20 +3,20 @@
 
 ## Change Mirror
 if [ -f /etc/fedora-release ]; then
-	echo "Fedora Confirmed"
-	echo "Change mirror > KAIST_fedora"
+	echo "Fedora Detected"
+	echo "Change mirror >>> KAIST_fedora"
     wget https://gist.githubusercontent.com/kimlulz/e8c9d0c9e2577d4d34819292d233985b/raw/d55ba1d631d3b04a0d5b8554cab354f15d5d2ccc/change-fedora-mirror.sh
     sudo sh change-fedora-mirror.sh
 
 elif [ -f /etc/rocky-release ]; then
-	echo "Rocky Linux Confirmed"
-	echo "Change mirror > NaverCloud_Rocky"
+	echo "Rocky Linux Detected"
+	echo "Change mirror >>> NaverCloud_Rocky"
 	wget https://gist.githubusercontent.com/kimlulz/742b304736d48a569bcc9be71113c294/raw/6c964cf843d05883f8f4eb438af33fa59a04f84d/change-rocklinux-mirror.sh
 	sudo sh change-rocklinux-mirror.sh
 	
 elif [ -f /etc/centos-release ]; then
-	echo "CentOS Confirmed"
-	echo "Change mirror > KAKAO_CentOS"
+	echo "CentOS Detected"
+	echo "Change mirror >>> KAKAO_CentOS"
     wget https://gist.githubusercontent.com/kimlulz/f8b98bf6d2ee21332ee4d183030f55a2/raw/7c503726b5c234beb576d7c85a3a683cc1cc2999/change-centos-mirror.sh
     sudo sh change-centos-mirror.sh -k
 else 
