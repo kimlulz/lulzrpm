2dc16fcb0dbb5f66a0572a0d6586d85ca2376cf5
kimlulz <kimlulz@naver.com>
2021-11-11 10:04:55 +0900

    Updated Script

--- lulzrpm.sh
+++ lulzrpm.sh
@@ -99,7 +99,7 @@ echo "DNF@lulzrpm $ Install VSCode from MS YUM_Repo"
 echo ""
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
+sudo dnf check-update && sudo dnf upgrade
 sudo dnf install -y code
 
 echo $USERNAME"@lulzrpm $ Customize .bashrc"
